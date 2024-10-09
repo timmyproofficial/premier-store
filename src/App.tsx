e6243a1a123fde35d7382e59a13dbf3f25a84484
timmyproofficial <timmyshow96@gmail.com>
2024-10-09 17:03:10 +0100

    Build the feature component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-primary-color">
+        <div className="flex flex-col items-center">
+          <p className="text-[2.2rem] font-[700] text-white">
+            Nationwide Delivery
+          </p>
+          <small className="text-white-accent text-[1.5rem] mt-[-8px]">
+            We deliver to anywhere
+          </small>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <p className="text-[2.2rem] font-[700] text-white">
+            Nationwide Delivery
+          </p>
+          <small className="text-white-accent text-[1.5rem] mt-[-8px]">
+            We deliver to anywhere
+          </small>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <p className="text-[2.2rem] font-[700] text-white">
+            Nationwide Delivery
+          </p>
+          <small className="text-white-accent text-[1.5rem] mt-[-8px]">
+            We deliver to anywhere
+          </small>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Feature.tsx
@@ -0,0 +1,12 @@
+const Feature = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <p className="text-[2.2rem] font-[700] text-white">Nationwide Delivery</p>
+      <small className="text-white-accent text-[1.5rem] mt-[-8px]">
+        We deliver to anywhere
+      </small>
+    </div>
+  );
+};
+
+export default Feature;
