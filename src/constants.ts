538266e1ce42bd095fced63948449194a04774cc
timmyproofficial <timmyshow96@gmail.com>
2024-10-11 12:02:38 +0100

    create constant for feature products.

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,35 @@
+import arrival1 from './assets/arrival1.png';
+import arrival2 from './assets/arrival2.png';
+import arrival3 from './assets/arrival3.png';
+import arrival4 from './assets/arrival4.png';
 
+export const productsFeature = [
+  {
+    id: 1,
+    title: 'Golden Ring',
+    img: arrival1,
+    price: 20,
+    discount: 10,
+  },
+  {
+    id: 2,
+    title: 'Ice wriste',
+    img: arrival2,
+    price: 30,
+    disount: 15,
+  },
+  {
+    id: 3,
+    title: 'Ice Necklace',
+    img: arrival3,
+    price: 50,
+    discount: 15,
+  },
+  {
+    id: 4,
+    title: 'Silver watch',
+    img: arrival4,
+    price: 45,
+    discount: 13,
+  },
+];
