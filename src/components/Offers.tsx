28f3b557862004ea08f9969ee8e4ed7702702682
timmyproofficial <timmyshow96@gmail.com>
2024-10-15 11:32:02 +0100

    Increase the height of offer block component for larger screen.

--- src/components/Offers.tsx
+++ src/components/Offers.tsx
@@ -3,7 +3,7 @@ import Offer from './offer';
 
 const Offers = () => {
   return (
-    <div className="bg-primary-color grid gap-y-20 md:grid-cols-2 lg:grid-cols-4 items-end px-24 py-6 min-h-[160px]">
+    <div className="bg-primary-color grid gap-y-20 md:grid-cols-2 lg:grid-cols-4 items-end px-24 py-16 md:min-h-[260px]">
       {offers.map((offer) => (
         <Offer key={offer.id} heading={offer.heading} text={offer.text} />
       ))}
