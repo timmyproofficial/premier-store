c7f590300f5d736193d89ab83a792c4f3c864c80
timmyproofficial <timmyshow96@gmail.com>
2024-10-15 15:39:49 +0100

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import FooterBlock from './components/FooterBlock';
 import { HomePage } from './pages';
 
 function App() {
   return (
     <div>
       <HomePage />
-      {/* <FooterBlock /> */}
     </div>
   );
 }
