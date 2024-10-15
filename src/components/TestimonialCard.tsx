412b3f1f6a1409b524e0d49c32f88ab20605144b
timmyproofficial <timmyshow96@gmail.com>
2024-10-15 08:05:15 +0100

    Transform testimonial card to a reusable component.

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -1,7 +1,13 @@
 import userIcon from '../assets/user.svg';
 import starIcon from '../assets/star-filled.svg';
 
-const TestimonialCard = () => {
+interface Props {
+  username: string;
+  msg: string;
+  rating: number;
+}
+
+const TestimonialCard = ({ username, msg, rating }: Props) => {
   return (
     <div className="bg-gray-accent rounded-[30px] border border-secondary-color p-12 m-28 max-w-[300px] flex flex-col gap-12">
       <div className="flex justify-between items-center">
@@ -10,17 +16,13 @@ const TestimonialCard = () => {
         </span>
 
         <div className="flex items-center gap-2">
-          <span className="text-2xl">5</span>
+          <span className="text-2xl">{rating}</span>
           <img className="w-8 h-8" src={starIcon} alt="Star Icon" />
         </div>
       </div>
-      <p className="text-2xl">
-        Lorem ipsum, this is a dummy test. The primary objective of this project
-        is to create a user-friendly website where clients can seamlessly book
-        appointments and make secure payments.
-      </p>
+      <p className="text-2xl">{msg}</p>
       <p className="text-2xl font-[600] text-black border-l-[3px] border-primary-color px-4 py-1">
-        John Doe
+        {username}
       </p>
     </div>
   );
