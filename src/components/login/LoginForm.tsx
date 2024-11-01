79fed15620b3636d0bd16c7393bf08b3b52b18f4
timmyproofficial <timmyshow96@gmail.com>
2024-11-01 15:24:45 +0100

    Finish the login UI

---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+  return (
+    <div className="w-[70%] mx-auto">
+      <p className="text-gray-color mb-8">Welcome Back,</p>
+
+      <form action="" className="text-gray-color">
+        <div className="flex flex-col gap-12 mb-4">
+          <div>
+            <input
+              className="login-input"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+          </div>
+
+          <div>
+            <input
+              className="login-input"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between mb-12">
+          <div className="flex gap-5">
+            <input type="radio" id="remember" name="remember" />
+            <label className="text-[1.4rem]" htmlFor="remember">
+              Remember Me
+            </label>
+          </div>
+          <small className="text-[1.4rem]">Forgot Password?</small>
+        </div>
+
+        <div className="flex justify-center">
+          <button
+            className="bg-primary-color text-white-color text-[1.6rem] py-[0.7rem] px-[5.6rem] mb-12 rounded-[1.2rem]"
+            type="submit"
+          >
+            Log In
+          </button>
+        </div>
+
+        <div className="text-center text-[1.4rem]">
+          Don't have an account?{' '}
+          <Link to="/" className="text-primary-color">
+            Register here
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/components/login/LoginLeftPanel.tsx
+++ src/components/login/LoginLeftPanel.tsx
@@ -1,6 +1,12 @@
+import LoginForm from './LoginForm';
+
 const LoginLeftPanel = () => {
   return (
-    <div className="bg-block-color p-12 rounded-tl-[50px]">LoginLeftPanel</div>
+    <div className="bg-block-color rounded-tl-[50px]">
+      <h4 className="font-semibold pl-24 pt-32 mb-8">Log in to your Account</h4>
+
+      <LoginForm />
+    </div>
   );
 };
 
