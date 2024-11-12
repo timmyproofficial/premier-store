17b34d9a23ea39e1f7828e2dd1869e23c94c07dc
timmyproofficial <timmyshow96@gmail.com>
2024-11-12 15:15:44 +0100

    handle footer mobile responsiveness.

--- src/components/FooterTop.tsx
+++ src/components/FooterTop.tsx
@@ -5,7 +5,7 @@ import FooterNav from './FooterNav';
 
 const FooterTop = () => {
   return (
-    <div className="text-white-color grid grid-cols-[auto_1fr_1fr_1fr]">
+    <div className="text-white-color grid grid-rows[auto_1fr] md:grid-rows[1fr] grid-cols-[1fr_1fr_1fr] md:grid-cols-[auto_1fr_1fr_1fr]">
       <div className="flex flex-col justify-between">
         <img className="w-[150px]" src={premierStoreImg} alt="Premier Store" />
         <h4>It's all about fashion & lifestyle</h4>
@@ -20,7 +20,7 @@ const FooterTop = () => {
         <FooterNav heading="Info" pages={infoPages} />
       </div>
 
-      <div>
+      <div className="col-span-4 md:col-span-1">
         <FooterNav
           heading="Contact"
           pages={contactPage}
