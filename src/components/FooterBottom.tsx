9ae9eb058e2667ff48fc29a5afaa1f61746c07e7
timmyproofficial <timmyshow96@gmail.com>
2024-11-12 17:16:37 +0100

    Handle footer mobile responsiveness.

--- src/components/FooterBottom.tsx
+++ src/components/FooterBottom.tsx
@@ -2,7 +2,7 @@ const FooterBottom = () => {
   return (
     <div className="pt-16 flex flex-col">
       <div className="bg-gray-color w-[95%] h-[0.2rem] mx-auto"></div>
-      <p className="text-[1.6rem] text-gray-color mt-9 self-center md:self-end">
+      <p className="text-[1.6rem] text-gray-color mt-9 self-center lg:self-end">
         Copyright &copy; 2024, All Rights Reserved.
       </p>
     </div>
--- src/components/FooterTop.tsx
+++ src/components/FooterTop.tsx
@@ -5,22 +5,22 @@ import FooterNav from './FooterNav';
 
 const FooterTop = () => {
   return (
-    <div className="text-white-color grid grid-rows[auto_1fr] md:grid-rows[1fr] grid-cols-[1fr_1fr_1fr] md:grid-cols-[auto_1fr_1fr_1fr]">
+    <div className="text-white-color grid grid-rows[auto_1fr] lg:grid-rows[1fr] grid-cols-[1fr_1fr_1fr] md:grid-cols-[auto_1fr_1fr_1fr]">
       <div className="flex flex-col justify-between">
         <img className="w-[150px]" src={premierStoreImg} alt="Premier Store" />
         <h4>It's all about fashion & lifestyle</h4>
         <SocialPlatforms />
       </div>
 
-      <div className="justify-self-center">
+      <div className="justify-self-end">
         <FooterNav heading="Pages" pages={pages} />
       </div>
 
-      <div>
+      <div className="col-span-2 justify-self-center lg:col-span-1 lg:justify-self-center">
         <FooterNav heading="Info" pages={infoPages} />
       </div>
 
-      <div className="mt-14 md:mt-0 col-span-4 md:col-span-1">
+      <div className="mt-14 lg:mt-0 col-span-4 lg:col-span-1">
         <FooterNav
           heading="Contact"
           pages={contactPage}
